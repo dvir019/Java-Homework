f0c0578e08e6b2c99ec22f50c1b354f31669c7ce
dvir <dvir019@gmail.com>
2019-09-14 21:59:35 +0300

    Start Mission_4

---
+++ Mission_4/src/app/BankAccount.java
@@ -0,0 +1,9 @@
+package app;
+
+public class BankAccount {
+	private String accountName;
+	private String accountNum;
+	private double balance;
+	private boolean isOverDraftAllowed;
+	private double overDraftSum;
+}
